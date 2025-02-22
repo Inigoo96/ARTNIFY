10a2bb70cc6522339b20b2ba3f99b1cd749f5163
inigoo96 <inigoal96@gmail.com>
2025-02-22 19:25:15 +0100

    Migrado backend a Express

--- ARTNIFY/back/index.js
+++ ARTNIFY/back/index.js
@@ -1,21 +1,13 @@
-const http = require('http');   // importar el modulo HTTP
+const express = require('express'); // Importamos Express
+const app = express(); // Creamos la aplicación
+const PORT = 3000; // Puerto del servidor
 
-const PORT = 3000;  // puerto en el que se ejecutará el servidor
-
-const server = http.createServer((req, res) => {
-    if(req.method === '/') {   //Ruta principal
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Servidor ARTNIFY en marcha');
-    } else if(req.url === '/api') { //Nuevas rutas
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({message: 'Bienvenido a la API de ARTNIFY'}));
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Error 404: Ruta no encontrada');
-    }
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('Servidor ARTNIFY con Express');
 });
 
 // Iniciar el servidor
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Servidor ARTNIFY corriendo en http://localhost:${PORT}/`);
 });
