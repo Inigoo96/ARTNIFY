ee5e1f0d6b3fc58f2ea17ae5594fceda8f01ef45
inigoo96 <inigoal96@gmail.com>
2025-02-22 19:03:11 +0100

    index.js optimizado

--- ARTNIFY/back/index.js
+++ ARTNIFY/back/index.js
@@ -1,13 +1,21 @@
-//importar el modulo HTTP de index.js
-const http = require('http');
+const http = require('http');   // importar el modulo HTTP
+
+const PORT = 3000;  // puerto en el que se ejecutará el servidor
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Servidor ARTNIFY en marcha');
+    if(req.url === '/') {   //Ruta principal
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Servidor ARTNIFY en marcha');
+    } else if(req.url === '/api') { //Nuevas rutas
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({message: 'Bienvenido a la API de ARTNIFY'}));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Error 404: Ruta no encontrada');
+    }
 });
 
-//iniciar el servidor en el peurto 3000
-server.listen(3000, () => {
-    console.log('Servidor ARTNIFY corriendo en el http://localhost:3000/');
-})
-
-
+// Iniciar el servidor
+server.listen(PORT, () => {
+    console.log(`Servidor ARTNIFY corriendo en http://localhost:${PORT}/`);
+});
